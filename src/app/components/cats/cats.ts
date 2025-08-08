e3c360a39a1ab527fb5298f730a1282fbcaf40cf
Mateo Ramirez Uribe <mateo.ramirez.uribe@gmail.com>
2025-08-08 16:16:23 -0500

    se implementa el listado de razas y el select con la funcionalidad de seleccionar una raza

--- src/app/components/cats/cats.ts
+++ src/app/components/cats/cats.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Cat } from '../../services/cat';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cats',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cats.html',
   styleUrl: './cats.scss'
 })
-export class Cats {
+export class Cats implements OnInit {
+  private catsService = inject(Cat);
 
+  breeds = this.catsService.breeds;
+  selectedBreed = this.catsService.selectedBreed;
+  isLoading = this.catsService.isLoading;
+  breedsError = this.catsService.breedsError;
+  hasBreeds = this.catsService.hasBreads;
+
+  ngOnInit(): void {
+    this.catsService.loadBreeds();
+  }
+
+  onSelectBreed(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+
+    if(target.value && target.value !== 'null') {
+      this.catsService.loadSelectedBreed(target.value);
+    }
+  }
 }
--- src/app/services/cat.ts
+++ src/app/services/cat.ts
@@ -1,7 +1,8 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, finalize, Observable, of, pipe } from 'rxjs';
 import { Environment } from './environment';
+import { Breed } from '../interfaces/breed.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,51 @@ export class Cat {
   private http = inject(HttpClient);
   private envService = inject(Environment);
 
+  // Estados privados
+  private _breeds = signal<Breed[]>([]);
+  private _selectedBreed = signal<Breed | null>(null);
+
+  // loadings
+  private _isLoading = signal(false);
+
+  // errores
+  private _breedsError = signal<string | null>(null);
+
+  // Estados públicos
+  readonly breeds = this._breeds.asReadonly();
+  readonly selectedBreed = this._selectedBreed.asReadonly();
+  readonly isLoading = this._isLoading.asReadonly();
+  readonly breedsError = this._breedsError.asReadonly();
+
+  // Estados computados
+  readonly hasBreads = computed(() => this.breeds().length > 0);
+
+  getBreeds(limit: number = 10, page: number = 0): Observable<Breed[]> {
+    this._isLoading.set(true);
+    this._breedsError.set(null);
+    const url = `${this.envService.apiUrl}breeds?limit=${limit}&page=${page}`;
+
+    return this.http.get<Breed[]>(url).pipe(
+      catchError((error) => {
+        this._breedsError.set('Error cargando la lista de razas');
+        return of([]);
+      }),
+      finalize(() => {
+        this._isLoading.set(false);
+      })
+    );
+  }
+
+  loadBreeds(limit: number = 10, page: number = 0): void {
+    this.getBreeds(limit, page).subscribe(breeds => {
+      this._breeds.set(breeds);
+    });
+  }
+
+  loadSelectedBreed(breedId: string): void {
+    const breed = this._breeds().find(b => b.id === breedId);
+    if(breed) {
+      this._selectedBreed.set(breed);
+    }
+  }
 }
